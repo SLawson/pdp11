dbeb27de6a25b67285c2bfd538717069352245e7
dunscomb <duncomb@pdx.edu>
2014-01-20 22:37:35 -0800

    Added #defines for all the opcodes and Mnemonic

--- src/pdp11.h
+++ src/pdp11.h
@@ -18,7 +18,86 @@ using namespace std;
 #define REGISTERS       8       //Number of GPR's
 
 //OpCode Mnemonics
-//TODO Brennttt use #define's here
+//TODO Brennttt use #define's here lol nice rob hahahah
+
+//Double-operand instructions
+#define MOV     01 //dest = src
+#define CMP     02 //compute src - dest, set flags only
+#define BIT     03 //compute dest & src set flags only
+#define BIC     04 //dest &= ~src
+#define BIS     05 //Logical OR dest|=src
+#define ADD     06 //dest +=src
+#define MOVB    11 //dest = src
+#define CMPB    12 //compute src - dest, set flags only
+#define BITB    13 //compute dest & src set flags only
+#define BICB    14 //dest &= ~src
+#define BISB    15 //Logical OR dest|=src
+#define SUB     16 //dest -=src
+#define MUL     070 //(R,R+1)= R*src
+#define DIV     071 //(R,R+1) ÷ src; quotient in R, remainder in R+1
+#define ASH     072 //Arithmetic shift: R <<= src
+#define ASHC    073 //Arithmetic shift combined: (R,R+1) <<= src
+#define XOR     074 //Exclusive OR: dest ^= reg
+//op code 075 used for floating point
+//op code 076 used for system instructions
+#define SOB     077 //Subtract one branch: Decrement register
+
+//Single-operand instructions
+#define SWAB    0003//swap bytes
+#define Jump    004r//Jumps to subroutine
+#define Emu     104x//Emulator trap
+#define CLR     0050//Clear: dest = 0
+#define CLRB    1050//Clear: dest = 0
+#define COM     0051//Complement dest = ~dest
+#define COMB    1051//Complement dest = ~dest
+#define INC     0052//Increment: dest +=1
+#define INCB    1052//Increment:dest +=1
+#define DEC     0053//Decrement: dest -=1
+#define DECB    1053//Decrement: dest -=1
+#define NEG     0054//Negate: dest = −dest
+#define NEGB    1054//Negate: dest = −dest
+#define ADC     0055//Add carry: dest += C
+#define ADCB    1055//Add carry: dest += C
+#define SBC     0056//Subtract carry: dest −= C
+#define SBCB    1056//Subtract carry: dest −= C
+#define TST     0057//Test: Load src, set flags only
+#define TSTB    1057//Test: Load src, set flags only
+#define ROR 	0060//Rotate right 1 bit
+#define RORB    1060//Rotate right 1 bit
+#define ROL	    0061//Rotate left 1 bit
+#define ROLB    1061//Rotate left 1 bit
+#define ASR	    0062//Shift right: dest >>= 1
+#define ASRB    1062//Shift right: dest >>= 1
+#define ASL	    0063//Shift left: dest <<= 1
+#define ASLB    1063//Shift left: dest <<= 1
+#define MARK    0064//Return from subroutine, skip 0..63 instruction words
+#define MTPS	1064//Move to status: PS = src
+#define MFPI	0065//Move from previous I space: −(SP) = src
+#define MFPD	1065//Move from previous D space: −(SP) = src
+#define MTPI	0066//Move to previous I space: dest = (SP)+
+#define MTPD	1066//Move to previous D space: dest = (SP)+
+#define SXT	    0067//Sign extend: dest = (16 copies of N flag)
+#define MFPS	1067//Move from status: dest = PS
+
+//Conditional branch instructions
+#define BR      0004xx//Branch unconditionally
+#define BNE     0010xx//Branch if not equal (Z=0)
+#define BEQ     0014xx//Branch if equal (Z=1)
+#define BGE     0020xx//Branch if greater that or equal (N|V = 0)
+#define BLT     0024xx//Branch if less than (N|V = 1)
+#define BGT     0030xx//Branch if greater than (N^V = 1)
+#define BLE     0034xx//Branch if less than or equal (N^V = 0)
+#define BPL     1000xx//Branch if plus (N=0)
+#define BMI     1004xx//Branch if minus (N=1)
+#define BHI     1010xx//Branch if higher than (C|Z = 0)
+#define BLOS    1014xx//Branch if lower or same (C|Z = 1)
+#define BVC     1020xx//Branch if overflow clear (V=0)
+#define BVS	    1024xx//Branch if overflow set (V=1)
+#define BCC     1030xx//Branch if carry clear (C=0)
+#define BHIS    1030xx//Branch if higher or same (C=0)
+#define BCS     1034xx//Branch if carry set (C=1)
+#define BLO	    1034xx//Branch if lower than (C=1)
+
 
 //General Purpose Register names
 #define R0      0
