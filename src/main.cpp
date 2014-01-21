67665cb4bab958c8ac7d4c98007c9f182f5cf1fe
Rob <rob@rlgaskell.com>
2014-01-20 17:49:58 -0800

    Set up as a C++ project rather than makefile project. Added messages to indicate start/end of sim.

--- src/main.cpp
+++ src/main.cpp
@@ -15,22 +15,25 @@ int main() {
 
 //Variables
 instruction current_inst = {0,0,0,0,0,0};       //Fetched/decoded instruction
-PSW Status_word = {0, FALSE, FALSE, FALSE, FALSE, FALSE};       //Current PSW
+PSW Status_word = {0, false, false, false, false, false};       //Current PSW
 int RAM[MEM_SIZE];                              //Contents of main memory
 int GPR[REGISTERS];                             //General Purpose Registers
+
+  cout <<"PDP-11/20 Simulation\n\n";
 
   //Initialize program
   initialize(RAM); //Read ASCII file, write code and static data to memory
 
   //Execute instructions
   while(current_inst.opcode) {
-      Fetch_decode(RAM, current_inst);
+      Fetch_Decode(RAM, current_inst);
       Operation(current_inst, GPR, Status_word);
 
       if(current_inst.write_flag)       //Write to RAM if write_flag set
         Write_mem(current_inst.result, current_inst.dest_addr);
   }
 
+  cout <<"\nPDP-11/20 Simulation Complete\n";
   cin.get();    //Pause before exiting
   return 0;
 }
